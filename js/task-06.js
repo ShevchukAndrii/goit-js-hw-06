a3c15b9fe260031e33a7407aba4da1924a0ed46f
ShevchukAndrii <andrefanchik@gmail.com>
2023-06-05 18:24:41 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,7 @@ const lengthInput = inputEl.getAttribute('data-length')
 inputEl.addEventListener('blur', (e) => {
     if (e.target.value.length === Number(lengthInput)) {
         inputEl.classList.add("valid")
+        inputEl.classList.remove("invalid")
     }
         else {
             inputEl.classList.add("invalid")
